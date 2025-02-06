7f26bdea1c662ceb7a28ba31680004050bd0d762
Archana Joshi <archana.u.joshi@gmail.com>
2025-02-06 16:07:46 -0500

    added analysis in file

--- DataCleaningAndAnalysis.sql
+++ DataCleaningAndAnalysis.sql
@@ -138,3 +138,68 @@ and t2.industry is not null;
 
 alter table layoffs_staging2 
 drop column row_num;
+
+-- -------------------------------------------actual analysis
+select * from layoffs_staging2;
+
+select max(total_laid_off), max(percentage_laid_off)
+from layoffs_staging2;
+
+select * from layoffs_staging2 
+where percentage_laid_off = 1 
+order by funds_raised_millions desc;
+
+select company, sum(total_laid_off) 
+from layoffs_staging2 
+group by company 
+order by 2 desc;
+
+-- analysing progression of layoffs over the month 
+
+select substr(`date`, 1, 7) as mon , sum(total_laid_off) 
+from layoffs_staging2 
+where substr(`date`, 1, 7) is not null 
+group by mon 
+order by 1 asc;
+
+with rolling_total as
+(
+select substr(`date`, 1, 7) as mon , sum(total_laid_off) as total_layoffs
+from layoffs_staging2 
+where substr(`date`, 1, 7) is not null 
+group by mon 
+order by 1 asc
+) 
+select mon, total_layoffs,  
+sum(total_layoffs) over(order by mon) as rolltotal 
+from rolling_total ;
+
+select company, YEAR(`date`) , sum(total_laid_off) 
+from layoffs_staging2 
+group by company, YEAR(`date`)  
+order by 3 DESC ;
+
+WITH company_year (company, years, total_laid_off) as 
+(
+select company, YEAR(`date`) , sum(total_laid_off) 
+from layoffs_staging2 
+group by company, YEAR(`date`)  
+) 
+select *, dense_rank() over (partition by years order by total_laid_off desc) as ranking 
+from company_year 
+where years is not null
+order by ranking;
+
+WITH company_year (company, years, total_laid_off) as 
+(
+select company, YEAR(`date`) , sum(total_laid_off) 
+from layoffs_staging2 
+group by company, YEAR(`date`)  
+), company_year_rank as 
+(
+select *, dense_rank() over (partition by years order by total_laid_off desc) as ranking 
+from company_year 
+where years is not null 
+) 
+select * from company_year_rank 
+where ranking <=5;
